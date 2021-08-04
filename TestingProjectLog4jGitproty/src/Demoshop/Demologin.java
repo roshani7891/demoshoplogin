4c2a147620e07d1a2eeaa600f25668a6f345cd13
roshani7891 <88184926+roshani7891@users.noreply.github.com>
2021-08-04 23:26:39 +0530

    Update Demologin.java

--- TestingProjectLog4jGitproty/src/Demoshop/Demologin.java
+++ TestingProjectLog4jGitproty/src/Demoshop/Demologin.java
@@ -37,9 +37,9 @@ public class Demologin {
 	  
 	  driver.get(pro.getProperty("url"));
 	  driver.findElement(By.xpath("//*[@id='Email']")).sendKeys(pro.getProperty("email"));
-      driver.findElement(By.xpath("//*[@id='Password']")).sendKeys(pro.getProperty("password"));
+          driver.findElement(By.xpath("//*[@id='Password']")).sendKeys(pro.getProperty("password"));
 	  driver.findElement(By.xpath("/html/body/div[4]/div[1]/div[4]/div[2]/div/div[2]/div[1]/div[2]/div[2]/form/div[5]/input")).click();
-	  System.out.println("pull project");
+	  system.out.println("pull project");
 	  
   }
 }
