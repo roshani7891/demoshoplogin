7fc0a07e01e53cb7fccdca1687e63dae37107acc
roshani7891 <88184926+roshani7891@users.noreply.github.com>
2021-08-05 22:34:28 +0530

    Update Demologin.java

--- TestingProjectLog4jGitproty/src/Demoshop/Demologin.java
+++ TestingProjectLog4jGitproty/src/Demoshop/Demologin.java
@@ -27,6 +27,7 @@ public class Demologin {
 
 	  System.setProperty("webdriver.chrome.driver", "E:\\driver\\New folder\\New folder\\chromedriver_win32\\chromedriver.exe");
       driver=new ChromeDriver();
+	  log.debug("open browser");
         }
 
   
@@ -34,13 +35,13 @@ public class Demologin {
   public void demologin() 
   {
 	  
-	  
+	  log.info("open url")
 	  driver.get(pro.getProperty("url"));
 	  driver.findElement(By.xpath("//*[@id='Email']")).sendKeys(pro.getProperty("email"));
           driver.findElement(By.xpath("//*[@id='Password']")).sendKeys(pro.getProperty("password"));
 	  driver.findElement(By.xpath("/html/body/div[4]/div[1]/div[4]/div[2]/div/div[2]/div[1]/div[2]/div[2]/form/div[5]/input")).click();
 	  System.out.println("pull project");
-	   system.out.println("pull project1");
+	   system.out.println("login succefully");
 	  
   }
 }
