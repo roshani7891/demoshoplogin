5dd39799a1ef5b2264bccabcec228f09706978df
admin <admin@Roshani>
2021-08-05 22:31:03 +0530

    ok

--- TestingProjectLog4jGitproty/src/Demoshop/Demologin.java
+++ TestingProjectLog4jGitproty/src/Demoshop/Demologin.java
@@ -8,6 +8,8 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
+import org.apache.log4j.Appender;
+import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -15,7 +17,7 @@ import org.testng.annotations.BeforeSuite;
 
 public class Demologin {
 	
-	static Logger log=Logger.getLogger()
+	static Logger log=Logger.getLogger(Demologin.class.getName());
   WebDriver driver;
   Properties pro=new Properties();
  
@@ -27,19 +29,19 @@ public class Demologin {
 
 	  System.setProperty("webdriver.chrome.driver", "E:\\driver\\New folder\\New folder\\chromedriver_win32\\chromedriver.exe");
       driver=new ChromeDriver();
+      log.debug("open url");
         }
 
   
   @Test
   public void demologin() 
   {
-	  
-	  
 	  driver.get(pro.getProperty("url"));
 	  driver.findElement(By.xpath("//*[@id='Email']")).sendKeys(pro.getProperty("email"));
       driver.findElement(By.xpath("//*[@id='Password']")).sendKeys(pro.getProperty("password"));
 	  driver.findElement(By.xpath("/html/body/div[4]/div[1]/div[4]/div[2]/div/div[2]/div[1]/div[2]/div[2]/form/div[5]/input")).click();
 	  System.out.println("pull project");
-	  
   }
+
 }
+
